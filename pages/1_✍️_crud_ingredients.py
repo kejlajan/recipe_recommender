1385baf5822c96f6df3e13f14303354dbc8888f3
Jan Kejla <redisperko@seznam.cz>
2025-05-22 22:30:00 +0200

    added spacy demo

--- pages/1_✍️_crud_ingredients.py
+++ pages/1_✍️_crud_ingredients.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import sqlite3
+import spacy
 import pandas as pd
+from rapidfuzz import fuzz
+nlp = spacy.load("en_core_web_sm")
 
 # Connect to the SQLite database
 conn = sqlite3.connect('food.db')
@@ -20,7 +23,34 @@ def fetch_ingredients_as_dict():
 def add_an_ingredient():
     st.write("to be done later")
 
+def get_fuzzy_scores(string1, string2):
+   return fuzz.ratio(string1.lower(), string2.lower())
+
+def check_duplicate_ingredients():
+    for ingredient in ingredients:
+        print("to be done")
+
+def lemmatize(string):
+    lemmatized = []
+    for token in nlp(string):
+        lemmatized.append(token.lemma_)
+    return lemmatized
+
+
+
+
+####################################################################################
+
+
+
 ingredients = fetch_ingredients_as_dict()
 
+with st.expander("All ingredients"):
+    st.write(ingredients)
 
-st.write(ingredients)
+with st.expander("Potential ingredient duplicates"):
+    st.write(get_fuzzy_scores("mashed potatoes", "potatoes"))
+
+with st.expander("Lemmatized words example"):
+    st.write(lemmatize("A very large dog was barking at honeyglazed bacon."))
+ 
